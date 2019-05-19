75449285c012f69b8302a1ef287239832d5f3112
Alexandre Roman <aroman@pivotal.io>
2019-05-19 15:44:05 +0200

    Init project

---
+++ src/main/java/fr/alexandreroman/eurodol/Application.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.eurodol;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.util.Currency;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+@RequiredArgsConstructor
+class CurrencyConverterController {
+    private static final Currency EUR_SYMBOL = Currency.getInstance("EUR");
+    private static final Currency USD_SYMBOL = Currency.getInstance("USD");
+    private final CurrencyConverterService ccs;
+
+    @GetMapping("/api/convert")
+    ResponseEntity<?> convert(@RequestParam(value = "amount", defaultValue = "1") BigDecimal amount,
+                              @RequestParam(value = "symbol", defaultValue = "EUR") String symbol) throws IOException {
+        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+            return ResponseEntity.badRequest().body("Amount must be positive: " + amount);
+        }
+        final Currency inputSymbol;
+        try {
+            inputSymbol = Currency.getInstance(symbol);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Invalid symbol: " + symbol);
+        }
+        final Currency outputSymbol;
+        if (EUR_SYMBOL.equals(inputSymbol)) {
+            outputSymbol = USD_SYMBOL;
+        } else if (USD_SYMBOL.equals(inputSymbol)) {
+            outputSymbol = EUR_SYMBOL;
+        } else {
+            return ResponseEntity.badRequest().body("Unsupported symbol: " + symbol);
+        }
+        return ResponseEntity.ok(ccs.convert(new CurrencyAmount(amount, inputSymbol), outputSymbol));
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class CurrencyConversion {
+    private CurrencyAmount input;
+    private CurrencyAmount output;
+    private BigDecimal exchangeRate;
+}
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+class CurrencyConverterService {
+    private final RestTemplate restTemplate;
+    @Value("${app.exchangeRatesEndpoint}")
+    private String exchangeRatesEndpoint;
+
+    public CurrencyConversion convert(CurrencyAmount input, Currency outputCurrency) throws IOException {
+        final String url = UriComponentsBuilder.fromHttpUrl(exchangeRatesEndpoint)
+                .path("/latest")
+                .queryParam("base", input.getSymbol().getCurrencyCode())
+                .queryParam("symbols", outputCurrency.getCurrencyCode())
+                .toUriString();
+
+        log.debug("Getting exchange rate between {} and {}", input.getSymbol(), outputCurrency);
+        final CurrencyResponse resp = restTemplate.getForObject(url, CurrencyResponse.class);
+        final BigDecimal exchangeRate = resp.rates.get(outputCurrency.getCurrencyCode());
+        if (exchangeRate == null) {
+            throw new IOException("Failed to get exchange rate");
+        }
+        log.debug("Got exchange rate between {} and {}: {}", input.getSymbol(), outputCurrency, exchangeRate);
+
+        final BigDecimal outputAmount = input.getAmount().multiply(exchangeRate);
+        final CurrencyAmount output = new CurrencyAmount(outputAmount, outputCurrency);
+        log.info("Converted using exchange rate {}: {} {} -> {} {}",
+                exchangeRate, input.getAmount(), input.getSymbol(), output.getAmount(), output.getSymbol());
+        return new CurrencyConversion(input, output, exchangeRate);
+    }
+
+    @Data
+    private static class CurrencyResponse {
+        private Map<String, BigDecimal> rates;
+    }
+}
+
+@Data
+@AllArgsConstructor
+class CurrencyAmount {
+    private final BigDecimal amount;
+    private final Currency symbol;
+}
+
+@Configuration
+class AppConfig {
+    @Bean
+    OkHttpClient httpClient() throws IOException {
+        // Setup an HTTP cache to reduce API calls to external endpoints.
+        final Cache cache = new Cache(Files.createTempDirectory("httpcache-").toFile(), 10 * 1024);
+        return new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .cache(cache)
+                .build();
+    }
+
+    @Bean
+    RestTemplate restTemplate(OkHttpClient httpClient) {
+        // Create a shared RestTemplate instance to make HTTP calls.
+        // We use OkHttp as the underlying transport library.
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory(httpClient));
+    }
+}
---
+++ src/test/java/fr/alexandreroman/eurodol/ApplicationTests.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.eurodol;
+
+import org.assertj.core.data.Percentage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Currency;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 9876)
+public class ApplicationTests {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private UriComponentsBuilder url() {
+        return UriComponentsBuilder.newInstance().scheme("http").host("localhost").port(port);
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testCurrencyConverterEndpoint() {
+        stubFor(get(urlEqualTo("/latest?base=USD&symbols=EUR"))
+                .willReturn(okJson("{\"base\":\"USD\",\"rates\":{\"EUR\":0.8950948801},\"date\":\"2019-05-17\"}")
+                        .withHeader(HttpHeaders.CACHE_CONTROL, "public, max-age=14400")
+                        .withHeader(HttpHeaders.EXPIRES,
+                                Instant.now().atOffset(ZoneOffset.UTC).plusSeconds(14400).format(DateTimeFormatter.RFC_1123_DATE_TIME))));
+
+        final String url = url().path("/api/convert").queryParam("amount", 1).queryParam("symbol", "USD").toUriString();
+        final ResponseEntity<CurrencyConversion> resp = restTemplate.getForEntity(url, CurrencyConversion.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(resp.getBody()).isNotNull();
+        final CurrencyConversion conv = resp.getBody();
+        assertThat(conv.getExchangeRate()).isCloseTo(BigDecimal.valueOf(0.8950948801), Percentage.withPercentage(5));
+        assertThat(conv.getInput()).isEqualTo(new CurrencyAmount(BigDecimal.ONE, Currency.getInstance("USD")));
+        assertThat(conv.getOutput()).isEqualTo(new CurrencyAmount(BigDecimal.valueOf(0.8950948801), Currency.getInstance("EUR")));
+
+        verify(1, getRequestedFor(urlEqualTo("/latest?base=USD&symbols=EUR")));
+    }
+
+    @Test
+    public void testCache() {
+        stubFor(get(urlEqualTo("/latest?base=USD&symbols=EUR"))
+                .willReturn(okJson("{\"base\":\"USD\",\"rates\":{\"EUR\":0.8950948801},\"date\":\"2019-05-17\"}")
+                        .withHeader(HttpHeaders.CACHE_CONTROL, "public, max-age=14400")
+                        .withHeader(HttpHeaders.EXPIRES,
+                                Instant.now().atOffset(ZoneOffset.UTC).plusSeconds(14400).format(DateTimeFormatter.RFC_1123_DATE_TIME))));
+
+        final String url = url().path("/api/convert").queryParam("amount", 1).queryParam("symbol", "USD").toUriString();
+        assertThat(restTemplate.getForObject(url, CurrencyConversion.class)).isNotNull();
+        assertThat(restTemplate.getForObject(url, CurrencyConversion.class)).isNotNull();
+        assertThat(restTemplate.getForObject(url, CurrencyConversion.class)).isNotNull();
+
+        verify(1, getRequestedFor(urlEqualTo("/latest?base=USD&symbols=EUR")));
+    }
+
+    @Test
+    public void testInvalidSymbol() {
+        final String url = url().path("/api/convert").queryParam("amount", 1).queryParam("symbol", "FOO").toUriString();
+        final ResponseEntity<String> resp = restTemplate.getForEntity(url, String.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testUnsupportedSymbol() {
+        final String url = url().path("/api/convert").queryParam("amount", 1).queryParam("symbol", "AUD").toUriString();
+        final ResponseEntity<String> resp = restTemplate.getForEntity(url, String.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testInvalidAmount() {
+        final String url = url().path("/api/convert").queryParam("amount", "A").queryParam("symbol", "EUR").toUriString();
+        final ResponseEntity<String> resp = restTemplate.getForEntity(url, String.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testDefaultInputValues() {
+        stubFor(get(urlEqualTo("/latest?base=EUR&symbols=USD"))
+                .willReturn(okJson("{\"base\":\"EUR\",\"rates\":{\"USD\":1.1172},\"date\":\"2019-05-17\"}")
+                        .withHeader(HttpHeaders.CACHE_CONTROL, "public, max-age=14400")
+                        .withHeader(HttpHeaders.EXPIRES,
+                                Instant.now().atOffset(ZoneOffset.UTC).plusSeconds(14400).format(DateTimeFormatter.RFC_1123_DATE_TIME))));
+
+        final String url = url().path("/api/convert").toUriString();
+        final ResponseEntity<CurrencyConversion> resp = restTemplate.getForEntity(url, CurrencyConversion.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(resp.getBody()).isNotNull();
+        final CurrencyConversion conv = resp.getBody();
+        assertThat(conv.getExchangeRate()).isCloseTo(BigDecimal.valueOf(1.1172), Percentage.withPercentage(5));
+        assertThat(conv.getInput()).isEqualTo(new CurrencyAmount(BigDecimal.ONE, Currency.getInstance("EUR")));
+        assertThat(conv.getOutput()).isEqualTo(new CurrencyAmount(BigDecimal.valueOf(1.1172), Currency.getInstance("USD")));
+
+        verify(1, getRequestedFor(urlEqualTo("/latest?base=EUR&symbols=USD")));
+    }
+
+    @Test
+    public void testHealthEndpoint() {
+        final ResponseEntity<String> resp = restTemplate.getForEntity("/actuator/health", String.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
