6770e953815d0bf9377d19c481aabe56c6c8284e
Alexandre Roman <aroman@pivotal.io>
2019-05-31 16:37:52 +0200

    Add Concourse pipeline

---
+++ ci/tasks/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e -x
+VERSION=`cat version/number`
+mkdir -p build
+cd repo
+./mvnw versions:set -DnewVersion=$VERSION && \
+./mvnw -Dmaven.repo.local=../.m2 -B package && cp target/*.jar ../build/$APP-$VERSION.jar && \
+echo "$APP $VERSION" > ../build/release && \
+cat > ../build/manifest.yml << EOF
+---
+applications:
+  - name: $APP
+    path: $APP-$VERSION.jar
+    random-route: true
+    health-check-type: http
+    health-check-http-endpoint: /actuator/health
+EOF
